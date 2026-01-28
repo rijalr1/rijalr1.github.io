672ef9c40b7af5fa7c09d39579a3ae4cf359f8ba
Ramesh Rijal <rameshrijal@Rameshs-MacBook-Pro.local>
2026-01-28 16:43:23 +1100

    added

---
+++ Mr Ramesh Rijal _ Find an Expert _ The University of Melbourne_files/utag.31.js
@@ -0,0 +1,5 @@
+//tealium universal tag - utag.31 ut4.0.202601220330, Copyright 2026 Tealium.com Inc. All Rights Reserved.
+window._kiq=window._kiq||[];try{(function(id,loader,u){u=utag.o[loader].sender[id]={};u.ev={'view':1};u.data={};u.data.jspath="cl.qualaroo.com/ki.js/65650/gTP.js";u.map={};u.extend=[function(a,b){try{if((typeof b['ut.event']!='undefined'&&b['ut.event'].toString().toLowerCase()=='view'.toLowerCase()&&typeof b['ga_virtual_page']=='undefined')){if(window.utagViewFired){return false;}}}catch(e){utag.DB(e)}}];u.send=function(a,b){if(u.ev[a]||typeof u.ev.all!="undefined"){for(c=0;c<u.extend.length;c++){try{d=u.extend[c](a,b);if(d==false)return}catch(e){}};var c,d,e,f;c=[];for(d in utag.loader.GV(u.map)){if(typeof b[d]!=="undefined"&&b[d]!==""){e=u.map[d].split(",");for(f=0;f<e.length;f++){u.data[e[f]]=b[d];}}}
+if(u.data.identify){window._kiq.push(["identify",u.data.identify]);}
+u.base_url="//"+u.data.jspath;u.s=document.getElementsByTagName("script")[0];u.scr=document.createElement("script");u.scr.type="text/javascript";u.scr.src=u.base_url;u.s.parentNode.insertBefore(u.scr,u.s);}}
+utag.o[loader].loader.LOAD(id);})('31','unimelb.main');}catch(e){}
